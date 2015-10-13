e4543b0988a9171a6221ce423905dc3bf821c172
Evan Prodromou <evan@fuzzy.io>
2015-10-12 17:31:08 -0700

    Fixup whitespace

--- dummy/consumer.py
+++ dummy/consumer.py
@@ -5,7 +5,6 @@ from getopt import getopt
 
 opts, args = getopt(sys.argv[1:], [], ['type', 'actor-id', 'object-id'])
 
-
 if opts[0][0] == '--type':
     print "Create"
 elif opts[0][0] == '--actor-id':
